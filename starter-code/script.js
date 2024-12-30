3a97c29d63cfe833c66e590968a6dc62c8a2aa24
ally-glenday <66145089+ally-glenday@users.noreply.github.com>
2024-12-30 11:22:52 +0900

    Add text toggle & JS

---
+++ starter-code/script.js
@@ -0,0 +1,13 @@
+var text = document.getElementById("toggle-text");
+
+
+function overviewText(){
+ text.innerHTML = "Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets. Mercury is one of four terrestrial planets in the Solar System, and is a rocky body like Earth.";
+}
+
+function internalStructureText(){
+ text.innerHTML = "Mercury appears to have a solid silicate crust and mantle overlying a solid, iron sulfide outer core layer, a deeper liquid core layer, and a solid inner core. The planet's density is the second highest in the Solar System at 5.427 g/cm3 , only slightly less than Earth's density.";  
+}
+function surfaceGeologyText(){
+  text.innerHTML = "Mercury's surface is similar in appearance to that of the Moon, showing extensive mare-like plains and heavy cratering, indicating that it has been geologically inactive for billions of years. It is more heterogeneous than either Mars's or the Moon’s.";  
+}
